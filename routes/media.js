d56602367e568adc10f96d27aa21014503fecdd9
rdy24 <raflizardeswandiy24@gmail.com>
2022-07-11 10:10:36 +0700

    first set up & create upload image, get list image, and delete image

---
+++ routes/media.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const isBase64 = require("is-base64");
+const base64Img = require("base64-img");
+const fs = require("fs");
+
+const { Media } = require("../models");
+
+// get list image
+router.get("/", async (req, res) => {
+  try {
+    const media = await Media.findAll({
+      attributes: ["id", "image"],
+    });
+    const mappedMedia = media.map((m) => {
+      m.image = `${req.get("host")}/${m.image}`;
+      return m;
+    });
+    return res.json({
+      status: "success",
+      data: mappedMedia,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// post image to server
+router.post("/", async (req, res) => {
+  const image = req.body.image;
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: "error", message: "invalid base64" });
+  }
+  base64Img.img(image, "./public/images", Date.now(), async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({ status: "error", message: err.message });
+    }
+    const filename = filepath.split("\\").pop().split("/").pop();
+    const media = await Media.create({ image: `images/${filename}` });
+    return res.json({
+      status: "success",
+      data: {
+        id: media.id,
+        image: `${req.get("host")}/images/${filename}`,
+      },
+    });
+  });
+});
+
+// delete image
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const media = await Media.findByPk(id);
+    if (!media) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "media not found" });
+    }
+    fs.unlink(`./public/${media.image}`, async (err) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+      await media.destroy();
+      return res.json({
+        status: "success",
+        message: "media deleted",
+      });
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
